f212d260ad57cf1bf7f32b848d75abbb0f151d25
Samuel-Berin <berin.s@husky.neu.edu>
2018-11-21 12:34:44 -0500

    Added swaps and deletions

--- lib/match_game/game.ex
+++ lib/match_game/game.ex
@@ -15,11 +15,47 @@ defmodule MatchGame.Game do
     board_size = :width * :height
     # A board of empty slots
     board = List.duplicate(0, board_size)
-    Enum.each(0..board_size, f(x) ->
+    Enum.each(0..board_size - 1, f(x) ->
       val = Enum.random(choices)
-      List.insert_at(board, x - 1, val
+      List.replace_at(board, x - 1, val)
     end)
     board
+  end
+
+  #Swaps the values at p1 and p2 if they are adjacent
+  def swap_vals(p1, p2) do
+    acceptable_indexs = [p1 + 1, p1 - 1, p1 - :width, p1 + :width]
+    if Enum.member?(acceptable_indexs, p2) do
+      val1 = Enum.at(:board, p1)
+      val2 = Enum.at(:board, p2)
+      List.replace_at(:board, p1, val2)
+      List.replace_at(:board, p2, val1)
+    end
+  end
+
+
+  def apply_deletions() do
+    board_size - :width * :height
+    Enum.each(0..board_size - 1, f(x) ->
+      # If we are supposed to delete this tile, do it
+      if Enum.member?(:to_delete, x) do
+        List.replace_at(:board, x, 0)
+        List.delete(:to_delete, x)
+      end
+
+      if Enum.at(:board, x) == 0 do
+        # If we are not at the top
+        if (x + :width < board_size) do
+          upperVal = Enum.at(:board, x + :width)
+          List.replace_at(:board, x, upperVal)
+          List.replace_at(:board, x + :width, 0)
+        else
+          newVal = Enum.random([1, 2, 3, 4, 5])
+          List.replace_at(:board, x, newVal)
+        end
+      end
+
+    end)
   end
 
   #Checks to see if the cell at x,y is at the center of
