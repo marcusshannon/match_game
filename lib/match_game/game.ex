c81fc610e6994e9275448dd3c53c6528c2058ae3
Samuel-Berin <berin.s@husky.neu.edu>
2018-11-21 21:27:08 -0500

    Multiplayer support, combos, basic loop resolved

--- lib/match_game/game.ex
+++ lib/match_game/game.ex
@@ -2,11 +2,13 @@ defmodule MatchGame.Game do
 
   def new do
     %{
-      score: 0,
+      score: [],
       width: 5,
       height: 10,
       board: new_board(),
-      to_delete: []
+      to_delete: [],
+      players: [],
+      active_player: 0
     }
   end
 
@@ -22,24 +24,39 @@ defmodule MatchGame.Game do
   end
 
   #Swaps the values at p1 and p2 if they are adjacent
-  def swap_vals(p1, p2) do
-    acceptable_indexs = [p1 - :width, p1 + :width]
-    if (rem(p1, :width) != 0) and (rem(p1, :width) != :width - 1) do
-      acceptable_indexs ++ [p1 - 1, p1 + 1]
-    # Handles the case when p1 is on the edge of a row
-    else
-      if rem(p1, :width) == 0 do
-        acceptable_indexs ++ [p1 + 1]
+  def swap_vals(p1, p2, playernum) do
+    if playernum == :active_player do
+      acceptable_indexs = [p1 - :width, p1 + :width]
+      if (rem(p1, :width) != 0) and (rem(p1, :width) != :width - 1) do
+        acceptable_indexs ++ [p1 - 1, p1 + 1]
+        # Handles the case when p1 is on the edge of a row
       else
-        acceptable_indexs ++ [p1 - 1]
+        if rem(p1, :width) == 0 do
+          acceptable_indexs ++ [p1 + 1]
+        else
+          acceptable_indexs ++ [p1 - 1]
+        end
       end
+      if Enum.member?(acceptable_indexs, p2) do
+        val1 = Enum.at(:board, p1)
+        val2 = Enum.at(:board, p2)
+        List.replace_at(:board, p1, val2)
+        List.replace_at(:board, p2, val1)
+      end
+
+      check_for_stability(1)
     end
-    if Enum.member?(acceptable_indexs, p2) do
-      val1 = Enum.at(:board, p1)
-      val2 = Enum.at(:board, p2)
-      List.replace_at(:board, p1, val2)
-      List.replace_at(:board, p2, val1)
+  end
+
+  #Resolves as many falls as necesary
+  #If turn based, advances the turns
+  def check_for_stability(combo) do
+    is_stable = check_board_for_matches()
+    if not is_stable do
+      dels = apply_deletions(combo)
+      check_for_stability(combo + 1)
     end
+    new_active_player = rem(:active_player + 1, length(:players))
   end
 
   #Generates a new cell at the index with no matches
@@ -62,14 +79,15 @@ defmodule MatchGame.Game do
 
 
   #Deletes all the cells we have to delete
-  def apply_deletions() do
+  def apply_deletions(combo) do
     board_size = :width * :height
     Enum.each(0..board_size - 1, fn(x) ->
       # If we are supposed to delete this tile, do it
       if Enum.member?(:to_delete, x) do
         List.replace_at(:board, x, 0)
         List.delete(:to_delete, x)
-        :score = :score + 1
+        current_score = Enum.at(:score, :active_player)
+        List.replace_at(:score, :active_player, current_score + combo)
       end
 
       if Enum.at(:board, x) == 0 do
@@ -84,6 +102,20 @@ defmodule MatchGame.Game do
       end
 
     end)
+  end
+
+  # Returns true if there are matches anywhere on the board
+  def check_board_for_matches() do
+    match_found = False
+    Enum.each(0...:height - 1, fn(y) ->
+      Enum.each(0...:width - 1, fn(x) ->
+        local_match = check_for_match(x, y)
+        match_found = match_found or local_match
+      end)
+
+    end)
+
+    match_found
   end
 
   #Checks to see if the cell at x,y is at the center of
