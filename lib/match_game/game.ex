7ff548ca5d83d8199c6222ff4bc78e9d581ea4a4
Samuel-Berin <berin.s@husky.neu.edu>
2018-11-24 18:26:39 -0500

    Passing game state

--- lib/match_game/game.ex
+++ lib/match_game/game.ex
@@ -5,7 +5,7 @@ defmodule MatchGame.Game do
       score: [],
       width: 5,
       height: 10,
-      board: new_board(),
+      board: new_board(5, 10),
       to_delete: [],
       players: [],
       active_player: 0
@@ -13,8 +13,8 @@ defmodule MatchGame.Game do
   end
 
   # Generates a new game board
-  def new_board() do
-    board_size = :width * :height
+  def new_board(width, height) do
+    board_size = width * height
     # A board of empty slots
     board = List.duplicate(0, board_size)
     Enum.each(0..board_size - 1, fn(x) ->
@@ -24,24 +24,24 @@ defmodule MatchGame.Game do
   end
 
   #Swaps the values at p1 and p2 if they are adjacent
-  def swap_vals(p1, p2, playernum) do
-    if playernum == :active_player do
-      acceptable_indexs = [p1 - :width, p1 + :width]
-      if (rem(p1, :width) != 0) and (rem(p1, :width) != :width - 1) do
+  def swap_vals(game, p1, p2, playernum) do
+    if playernum == game.active_player do
+      acceptable_indexs = [p1 - game.width, p1 + game.width]
+      if (rem(p1, game.width) != 0) and (rem(p1, game.width) != game.width - 1) do
         acceptable_indexs ++ [p1 - 1, p1 + 1]
         # Handles the case when p1 is on the edge of a row
       else
-        if rem(p1, :width) == 0 do
+        if rem(p1, game.width) == 0 do
           acceptable_indexs ++ [p1 + 1]
         else
           acceptable_indexs ++ [p1 - 1]
         end
       end
       if Enum.member?(acceptable_indexs, p2) do
-        val1 = Enum.at(:board, p1)
-        val2 = Enum.at(:board, p2)
-        List.replace_at(:board, p1, val2)
-        List.replace_at(:board, p2, val1)
+        val1 = Enum.at(game.board, p1)
+        val2 = Enum.at(game.board, p2)
+        List.replace_at(game.board, p1, val2)
+        List.replace_at(game.board, p2, val1)
       end
 
       check_for_stability(1)
@@ -50,52 +50,52 @@ defmodule MatchGame.Game do
 
   #Resolves as many falls as necesary
   #If turn based, advances the turns
-  def check_for_stability(combo) do
+  def check_for_stability(game, combo) do
     is_stable = check_board_for_matches()
     if not is_stable do
       dels = apply_deletions(combo)
       check_for_stability(combo + 1)
     end
-    new_active_player = rem(:active_player + 1, length(:players))
+    new_active_player = rem(game.active_player + 1, length(game.players))
   end
 
   #Generates a new cell at the index with no matches
-  def generate_new_cell_no_match(index) do
+  def generate_new_cell_no_match(game, index) do
     choices = [1, 2, 3, 4, 5]
-    if (index < :width) and (index > 0) do
-      left_val = Enum.at(:board, index - 1)
+    if (index < game.width) and (index > 0) do
+      left_val = Enum.at(game.board, index - 1)
       choices = List.delete(choices, left_val)
     else
-      left_val = Enum.at(:board, index - 1)
-      bottom_val = Enum.at(:board, index - :width)
+      left_val = Enum.at(game.board, index - 1)
+      bottom_val = Enum.at(game.board, index - game.width)
       choices = List.delete(choices, left_val)
       if List.member?(choices, bottom_val) do
         choices = List.delete(choices, bottom_val)
       end
     end
 
-    List.replace_at(:board, index, Enum.random(choices))
+    List.replace_at(game.board, index, Enum.random(choices))
   end
 
 
   #Deletes all the cells we have to delete
-  def apply_deletions(combo) do
-    board_size = :width * :height
+  def apply_deletions(game, combo) do
+    board_size = game.width * game.height
     Enum.each(0..board_size - 1, fn(x) ->
       # If we are supposed to delete this tile, do it
-      if Enum.member?(:to_delete, x) do
-        List.replace_at(:board, x, 0)
-        List.delete(:to_delete, x)
-        current_score = Enum.at(:score, :active_player)
-        List.replace_at(:score, :active_player, current_score + combo)
+      if Enum.member?(game.to_delete, x) do
+        List.replace_at(game.board, x, 0)
+        List.delete(game.to_delete, x)
+        current_score = Enum.at(game.score, game.active_player)
+        List.replace_at(game.score, game.active_player, current_score + combo)
       end
 
-      if Enum.at(:board, x) == 0 do
+      if Enum.at(game.board, x) == 0 do
         # If we are not at the top
-        if (x + :width < board_size) do
-          upperVal = Enum.at(:board, x + :width)
-          List.replace_at(:board, x, upperVal)
-          List.replace_at(:board, x + :width, 0)
+        if (x + game.width < board_size) do
+          upperVal = Enum.at(game.board, x + game.width)
+          List.replace_at(game.board, x, upperVal)
+          List.replace_at(game.board, x + game.width, 0)
         else
           generate_new_cell_no_match(x)
         end
@@ -105,10 +105,10 @@ defmodule MatchGame.Game do
   end
 
   # Returns true if there are matches anywhere on the board
-  def check_board_for_matches() do
+  def check_board_for_matches(game) do
     match_found = False
-    Enum.each(0...:height - 1, fn(y) ->
-      Enum.each(0...:width - 1, fn(x) ->
+    Enum.each(0..game.height - 1, fn(y) ->
+      Enum.each(0..game.width - 1, fn(x) ->
         local_match = check_for_match(x, y)
         match_found = match_found or local_match
       end)
@@ -120,29 +120,29 @@ defmodule MatchGame.Game do
 
   #Checks to see if the cell at x,y is at the center of
   #A match
-  def check_for_match(x, y) do
-    horizontal_match = check_for_horizontal_match(x, y)
-    vertical_match = check_for_vertical_match(x,y)
+  def check_for_match(game, x, y) do
+    horizontal_match = check_for_horizontal_match(game, x, y)
+    vertical_match = check_for_vertical_match(game, x,y)
     match = horizontal_match or vertical_match
     match
   end
 
   # Checks to see if there is a horizontal_match with the cell
   # at x,y, in the center
-  def check_for_horizontal_match(x, y) do
-    if (x == 0) or (x == :width - 1) do
+  def check_for_horizontal_match(game, x, y) do
+    if (x == 0) or (x == game.width - 1) do
       False
     else
-      index = (y * :width) + x
-      center_val = Enum.at(:board, index)
-      left_val = Enum.at(:board, index - 1)
-      right_val = Enum.at(:board, index + 1)
+      index = (y * game.width) + x
+      center_val = Enum.at(game.board, index)
+      left_val = Enum.at(game.board, index - 1)
+      right_val = Enum.at(game.board, index + 1)
       match = (center_val == left_val) and (center_val == right_val)
       #If we have a match, mark the tiles so we can delete them
       if match do
-        deletions = :to_delete ++ [index - 1, index, index + 1]
+        deletions = game.to_delete ++ [index - 1, index, index + 1]
         deletions = Enum.uniq(deletions)
-        :to_delete = deletions
+        game.to_delete = deletions
       end
       match
     end
@@ -150,18 +150,18 @@ defmodule MatchGame.Game do
 
   # Checks to see if there is a vertical_match with the cell
   # at x,y, in the center
-  def check_for_vertical_match(x, y) do
-    if (y == 0) or (y == :height - 1) do
+  def check_for_vertical_match(game, x, y) do
+    if (y == 0) or (y == game.height - 1) do
       False
     else
-      index = (y * :width) + x
-      center_val = Enum.at(:board, index)
-      bottom_val = Enum.at(:board, index - :width)
-      top_val = Enum.at(:board, index + :width)
+      index = (y * game.width) + x
+      center_val = Enum.at(game.board, index)
+      bottom_val = Enum.at(game.board, index - game.width)
+      top_val = Enum.at(game.board, index + game.width)
       match = (center_val == top_val) and (center_val == bottom_val)
       #If we have a match, mark the tiles so we can delete them
       if match do
-        deletions = :to_delete ++ [index - :width, index, index + :width]
+        deletions = game.to_delete ++ [index - game.width, index, index + game.width]
       end
       match
     end
